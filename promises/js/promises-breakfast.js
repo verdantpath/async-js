922910d7904dbd667e18005037c668b5b3644b45
verdantpath <m_dittmann@yahoo.com>
2019-07-25 21:35:26 -0700

    add reject and catch

--- promises/js/promises-breakfast.js
+++ promises/js/promises-breakfast.js
@@ -1,8 +1,8 @@
 const breakfastPromise = new Promise( ( resolve, reject) => { 
     setTimeout(() => {
-        resolve("Your order is ready, come and pick it up!");
+        reject("Uh oh, there was a problem with your order!");
     }, 3000);
 });
 
 console.log(breakfastPromise);
-breakfastPromise.then( val => console.log(val) );
+breakfastPromise.then( val => console.log(val) ).catch( err => console.log(err) );
