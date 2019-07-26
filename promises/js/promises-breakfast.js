2623ba867dacf290f3acf0292b0245b600fb5359
verdantpath <m_dittmann@yahoo.com>
2019-07-25 21:13:14 -0700

    add resolve

--- promises/js/promises-breakfast.js
+++ promises/js/promises-breakfast.js
@@ -1,7 +1,8 @@
 const breakfastPromise = new Promise( ( resolve, reject) => { 
     setTimeout(() => {
-
+        resolve("Your order is ready, come and pick it up!");
     }, 3000);
 });
 
 console.log(breakfastPromise);
+breakfastPromise.then( val => console.log(val) );
